4d4bbc3c02ff87d3f1d44d559b2bdf20df7dc20f
soulwill1 <willianverissimo88@gmail.com>
2025-10-09 17:10:07 -0300

    fixes on tests

--- app/db/database.py
+++ app/db/database.py
@@ -8,10 +8,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"check_same_thread": False}
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
