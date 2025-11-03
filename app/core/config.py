c02a7ee5e21ed8118f90dc1c1a97b90055590efb
soulwill1 <willianverissimo88@gmail.com>
2025-11-03 14:00:24 -0300

    feat: CORS added on main and core

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
 # for auth service
 API_V1_STR = "/api/v1"
 AUTH_LOGIN_ENDPOINT = "/auth/login"
@@ -9,3 +12,13 @@ AUTH_LOGIN_ENDPOINT = "/auth/login"
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def setup_cors(app: FastAPI):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # RESTRINGIR EM PROD
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from .db.database import Base, engine
 from .api.v1.router import api_router
+from .core.config import setup_cors
 
 
 app = FastAPI(title="HoraMed API")
 
 Base.metadata.create_all(bind=engine)
 
+
+setup_cors(app)
 app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
